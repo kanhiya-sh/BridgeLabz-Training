cf0521292a0b642021a82eb6bfb7c6fc33fc1204
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-18 15:18:05 +0530

    [kanhiya] Calculator Program

---
+++ core-java-practice/gcr-codebase/programming-elements/Calculator.java
@@ -0,0 +1,15 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        float number1 = 5.5f;
+        float number2 = 3.5f;
+
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication and division value of 2 numbers " + number1 + " and " + number2 + " is " + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
