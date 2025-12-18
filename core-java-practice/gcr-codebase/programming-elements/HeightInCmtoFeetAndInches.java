873dc3a8341f22a1dc25d0416237d61d99a98117
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-18 12:55:58 +0530

    [kanhiya] Height in Cm to Feet and Inches

---
+++ core-java-practice/gcr-codebase/programming-elements/HeightInCmtoFeetAndInches.java
@@ -0,0 +1,14 @@
+
+
+import java.util.Scanner;
+
+public class HeightInCmtoFeetAndInches {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double heightInCm = scanner.nextDouble();
+        double totalInches = heightInCm / 2.54;
+        int inFeet = (int) (totalInches / 12);
+        double inInches = totalInches % 12;
+        System.out.println("Your Height in cm is " + heightInCm + " while in feet is " + inFeet + " and inches is " + inInches);
+    }
+}
