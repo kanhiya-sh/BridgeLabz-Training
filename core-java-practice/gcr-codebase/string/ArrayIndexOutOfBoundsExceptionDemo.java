0ea8c6450faea796941c133a3252e336f0cfa366
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-23 16:18:35 +0530

    [kanhiya] fixed. ArrayIndexOutOfBoundsExceptionDemo.java

--- core-java-practice/gcr-codebase/string/ArrayIndexOutOfBoundsExceptionDemo.java
+++ core-java-practice/gcr-codebase/string/ArrayIndexOutOfBoundsExceptionDemo.java
@@ -6,40 +6,35 @@ public class ArrayIndexOutOfBoundsExceptionDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("How many names? ");
+        System.out.print("Names Count - ");
         int count = sc.nextInt();
         sc.nextLine();
 
         String[] names = new String[count];
 
         for (int i = 0; i < count; i++) {
-            System.out.print("Enter name " + (i + 1) + ": ");
+            System.out.print("Enter name " + (i + 1) + "- ");
             names[i] = sc.nextLine();
         }
 
-        System.out.println("\n--- Generating Exception ---");
-        generateArrayIndexException(names);
+        System.out.println("\n-- Generating Exception --");
+        String name = names[names.length];
 
-        System.out.println("\n--- Handling Exception ---");
+        System.out.println("\n-- Handling Exception --");
         handleArrayIndexException(names);
 
     }
 
-    public static void generateArrayIndexException(String[] nameArr) {
-        // Accessing index beyond array length
-        String name = nameArr[nameArr.length];
-    }
-
-    // Method to handle exception
+    // here we are writing a Method to handle exception
     public static void handleArrayIndexException(String[] nameArr) {
         try {
             String name = nameArr[nameArr.length];
             System.out.println("Name: " + name);
         }
         catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Caught ArrayIndexOutOfBoundsException: " + e.getMessage());
+            System.out.println("Found ArrayIndexOutOfBoundsException: " + e.getMessage());
         } catch (RuntimeException e) {
-            System.out.println("Caught generic RuntimeException: " + e.getMessage());
+            System.out.println("Found generic RuntimeException: " + e.getMessage());
         }
     }
 }
