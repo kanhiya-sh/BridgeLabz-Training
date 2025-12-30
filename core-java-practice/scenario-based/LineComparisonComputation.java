d087b2de659a0cadbfd06ebd18195b6867c55488
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-30 10:52:14 +0530

    [kanhiya] refactor. uc1 calculate line length

--- core-java-practice/scenario-based/LineComparisonComputation.java
+++ core-java-practice/scenario-based/LineComparisonComputation.java
@@ -8,7 +8,7 @@ public class LineComparisonComputation {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Line Comparison Computation Program");
 		
-//		now we are taking user inputs for the coordinates ( x1, x2, y1, y2)
+//		now we are taking user inputs for the coordinates ( x1, x2, y1, y2 )
 		System.out.println("Enter the Coordinates ( x1, x2, y1, y2 ) : ");
 		Scanner myScan = new Scanner(System.in);
 		int x1 = myScan.nextInt();
@@ -18,7 +18,7 @@ public class LineComparisonComputation {
 		
 //		now we are going to calculate the length of lines
 		double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        System.out.println("Length of line is : " + length);
+        System.out.println("Length of Line is : " + length);
 	}
 
 }
