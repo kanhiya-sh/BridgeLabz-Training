3f2f4dce3de134ca7486ccb8a7fa7c12c41e41e3
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-30 15:17:39 +0530

    [kanhiya] refactor. uc3 setting options for user

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -6,12 +6,30 @@ public class SnakeandLadder {
 		
 		System.out.println("Welcome to Snake and Ladder Game");
 		
-		int Position = 0;
-		System.out.println("Player starts at position " + Position);
+		int position = 0;
+		System.out.println("Player starts at position " + position);
 		
 //		now we are rolling the dice and using random the number between 1 to 6 is getting selected 
 		int dice = (int)(Math.random() * 6) + 1;
 		System.out.println("Dice rolled : " + dice);
+		
+//		now we are setting position for the user that where they will move to their position
+		int options = (int)(Math.random() * 3); 
+		System.out.println("You got the option : " + options);
+		// 0 - for no play , 1 - ladder , 2 - snake bite 
+		switch(options) {
+		case 0 :
+			System.out.println("No Play");
+			break;
+		case 1 :
+			position += dice ;
+			System.out.println("You Found a Ladder!! Moved to " + position);
+			break;
+		case 2 :
+			position -= dice;
+			 System.out.println("Bitten by Snake!! Move back to " + position);
+			 break;
+		}
 
 	}
 
