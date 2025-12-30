bf792b21921234431f0e32deea82b1dd040480a0
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-30 14:58:24 +0530

    [kanhiya] add. uc1 single player start at 0

---
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package scenarioBased;
+
+public class SnakeandLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake and Ladder Game");
+		
+		int Position = 0;
+		System.out.println("Player starts at position " + Position);
+
+	}
+
+}
