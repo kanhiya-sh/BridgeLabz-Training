095a86d81ea4f892b63ed6dcddb167b3508767ed
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-30 10:48:16 +0530

    [kanhiya] refactor. uc welcome message

--- core-java-practice/scenario-based/LineComparisonComputation.java
+++ core-java-practice/scenario-based/LineComparisonComputation.java
@@ -6,7 +6,19 @@ public class LineComparisonComputation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Line Comparison Computation Program"); 
+		System.out.println("Welcome to Line Comparison Computation Program");
+		
+//		now we are taking user inputs for the coordinates ( x1, x2, y1, y2)
+		System.out.println("Enter the Coordinates ( x1, x2, y1, y2 ) : ");
+		Scanner myScan = new Scanner(System.in);
+		int x1 = myScan.nextInt();
+		int x2 = myScan.nextInt();
+		int y1 = myScan.nextInt();
+		int y2 = myScan.nextInt();
+		
+//		now we are going to calculate the length of lines
+		double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        System.out.println("Length of line is : " + length);
 	}
 
 }
