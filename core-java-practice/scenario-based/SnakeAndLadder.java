e9ba692e4ecf0be76d59fa13cf6e794d692ea671
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-30 16:36:57 +0530

    [kanhiya] refactor. uc7 Two players for snake and ladder game

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -2,56 +2,89 @@ package scenarioBased;
 
 public class SnakeandLadder {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Welcome to Snake and Ladder Game");
-		
-		int position = 0;
-		System.out.println("Player starts at position " + position);
-		
-		int totalCount = 0;
-		
-		while(position < 100) { 
-//		now we are rolling the dice and using random the number between 1 to 6 is getting selected 
-			int dice = (int)(Math.random() * 6) + 1;
-			System.out.println("Dice rolled : " + dice);
-			
-			totalCount++; // increasing the count every time the dice is played 
-	
-//		now we are setting position for the user that where they will move to their position
-			int options = (int)(Math.random() * 3); 
-			System.out.println("You got the option : " + options);
-			// 0 - for no play , 1 - ladder , 2 - snake bite 
-	
-			if(options == 0) {
-				System.out.println("No Play");
-			}
-			if (options == 1) {
-				if( position + dice <= 100) {
-					position += dice;
-					System.out.println("You Found the Ladder! You Moved to " + position);
-				}
-				else {
-					System.out.println("Ladder found but cannot move as you can go beyond 100");
-				}
-			}
-		    else if (options == 2) {
-		    		position -= dice;
-		    		if (position < 0) {
-		    			position = 0;
-		    		}
-		    		
-		        System.out.println("Bitten by Snake! Moved back to " + position);
-		    }
-			
-			System.out.println("Current Position is : " + position);
-			System.out.println("----------------------");
-		    
-		}
-		
-		System.out.println("Game Finished!");
-        System.out.println("Total Dice Rolls : " + totalCount);
+    public static void main(String[] args) {
+        
+        System.out.println("Welcome to Snake and Ladder Game");
+        
+        // Players initial positions
+        int player1Position = 0;
+        int player2Position = 0;
+        
+        // Track whose turn it is (true for Player 1, false for Player 2)
+        boolean isPlayer1Turn = true; 
+        
+        int totalDiceRolls = 0;
+        
+        // Game continues until one of them reaches 100
+        while(player1Position < 100 && player2Position < 100) { 
+            
+            String currentPlayer = isPlayer1Turn ? "Player 1" : "Player 2";
+            int currentPosition = isPlayer1Turn ? player1Position : player2Position;
+            
+            System.out.println("\nTurn: " + currentPlayer + " (Position: " + currentPosition + ")");
+            
+            // Rolling the dice
+            int dice = (int)(Math.random() * 6) + 1;
+            System.out.println("Dice rolled : " + dice);
+            totalDiceRolls++;
+            
+            // Checking for options
+            int options = (int)(Math.random() * 3); 
+            // 0 - No Play, 1 - Ladder, 2 - Snake
+            
+            if(options == 0) {
+	            	// Position remains same
+                System.out.println("Option: No Play (Stay at same position)");
+            }
+            else if (options == 1) {
+                System.out.println("Option: Ladder !! (You Moved ahead)");
+                if(currentPosition + dice <= 100) {
+                    currentPosition += dice;
+                } 
+                else {
+                    System.out.println("Got Ladder but cannot move as position would exceed 100");
+                }
+            }
+            else if (options == 2) {
+                System.out.println("Option: Bitten by Snake !! (Move back)");
+                currentPosition -= dice;
+                if (currentPosition < 0) {
+                    currentPosition = 0;
+                }
+            }
+            
+            // Update the actual position of the playing player
+            if (isPlayer1Turn) {
+                player1Position = currentPosition;
+                System.out.println("Player 1 New Position: " + player1Position);
+            } else {
+                player2Position = currentPosition;
+                System.out.println("Player 2 New Position: " + player2Position);
+            }
+            
+            // Checking winning condition
+            if (currentPosition == 100) {
+                System.out.println("----------------------");
+                System.out.println("CONGRATULATIONS! " + currentPlayer + " WON THE GAME!");
+                break; // Break the loop immediately
+            }
 
-	}
-
+            // Turn switching
+            // If Option is Ladder (1), user plays again 
+            if (options == 1) {
+                System.out.println("User got Ladder! Plays again.");
+            } 
+            else {
+	            	if (isPlayer1Turn == true) {
+						isPlayer1Turn = false;
+					} 
+	            	else {
+						isPlayer1Turn = true;  
+					}
+	            }
+        		}
+       
+        System.out.println("Game Finished!");
+        System.out.println("Total Dice Rolls in the game: " + totalDiceRolls);
+    }
 }
