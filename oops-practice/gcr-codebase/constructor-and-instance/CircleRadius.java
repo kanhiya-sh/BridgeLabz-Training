ab9f0dea559cbf00b3e60aabf5d15b3528870f09
kanhiya-sh <sharmakanhiya288@gmail.com>
2025-12-31 22:29:11 +0530

    [kanhiya] add. CircleRadius.java

---
+++ oops-practice/gcr-codebase/constructor-and-instance/CircleRadius.java
@@ -0,0 +1,22 @@
+package constructorAndInstance;
+
+class Circle {
+    double radius;
+
+    Circle() {
+        this(1.0);
+    }
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+}
+
+public class CircleRadius {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
