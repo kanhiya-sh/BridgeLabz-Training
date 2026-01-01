c04f8a9a48494602a098e070220ced78e9efb428
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-01 19:17:42 +0530

    [kanhiya] Add. VehicleRegistrationSystem.java

---
+++ oops-practice/gcr-codebase/this-static-final-and-instance/VehicleRegistrationSystem.java
@@ -0,0 +1,36 @@
+package Capgemini_Day14_this_static_final;
+
+class Vehicle {
+    static double registrationFee = 150.0;
+    final String registrationNumber;
+    String ownerName;
+    String vehicleType;
+
+    Vehicle(String ownerName, String vehicleType, String registrationNumber) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+        this.registrationNumber = registrationNumber;
+    }
+
+    static void updateRegistrationFee(double fee) {
+        registrationFee = fee;
+    }
+
+    void display(Object obj) {
+        if (obj instanceof Vehicle) {
+            System.out.println("Owner Name: " + ownerName);
+            System.out.println("Vehicle Type: " + vehicleType);
+            System.out.println("Registration Number: " + registrationNumber);
+            System.out.println("Registration Fee: $" + registrationFee);
+        }
+    }
+}
+
+public class VehicleRegistrationSystem {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle("Honest raj", "Sedan", "ABC123");
+        Vehicle v2 = new Vehicle("Price danish", "SUV", "XYZ789");
+        v1.display(v1);
+        v2.display(v2);
+    }
+}
