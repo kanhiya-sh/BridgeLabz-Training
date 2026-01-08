0b1e79a0fd8c1509f2cb9a325eafb2fa3098226a
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-08 22:16:42 +0530

    [kanhiya] Add. ToLowerCase.java

---
+++ dsa-practice/leet-code-codebase/ToLowerCase.java
@@ -0,0 +1,12 @@
+package BridgeLabz_Day20_Leetcode;
+
+import java.util.Scanner;
+
+public class ToLowerCase {
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Enter a string to convert to lowercase:");
+        String str = myScan.nextLine();
+        System.out.println(str.toLowerCase());
+    }
+}
