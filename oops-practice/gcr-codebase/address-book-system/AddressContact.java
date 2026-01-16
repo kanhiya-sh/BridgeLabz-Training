59481aac60d1c456ee775682c1c8ac460cacb9d9
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-16 15:23:38 +0530

    [kanhiya] Add. UC-1 Address Contact details

---
+++ oops-practice/gcr-codebase/address-book-system/AddressContact.java
@@ -0,0 +1,24 @@
+package BridgeLabz_Day26_AddressBookProblem;
+
+class Contact {
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+    Contact(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state =state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
+
