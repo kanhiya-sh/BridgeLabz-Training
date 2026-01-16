76aecf352731d15f33e56893f85d30ab51decdb6
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-16 15:21:03 +0530

    [kanhiya] Add. This is the Presentation Layer (Main class)

---
+++ oops-practice/gcr-codebase/address-book-system/AddressBookSystemMain.java
@@ -0,0 +1,8 @@
+package BridgeLabz_Day26_AddressBookProblem;
+
+public class AddressBookSystemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program"); // starting
+
+    }
+}
