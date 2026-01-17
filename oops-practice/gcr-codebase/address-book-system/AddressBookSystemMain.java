f5cc0f299c1d2cfa0f72566c3efa8748be04a9d8
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-17 11:13:06 +0530

    [kanhiya] Refactor. adding user input to edit the contact

--- oops-practice/gcr-codebase/address-book-system/AddressBookSystemMain.java
+++ oops-practice/gcr-codebase/address-book-system/AddressBookSystemMain.java
@@ -1,11 +1,14 @@
 package BridgeLabz_Day26_AddressBookProblem;
 
 import java.util.*;
+
 public class AddressBookSystemMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program"); // starting
         Scanner myScan = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
+
+        // -------- UC-2 : Add Contact --------
         System.out.print("Enter First Name : ");
         String firstName = myScan.nextLine();
         System.out.print("Enter Last Name : ");
@@ -22,8 +25,35 @@ public class AddressBookSystemMain {
         String phoneNumber = myScan.nextLine();
         System.out.print("Enter Email : ");
         String email = myScan.nextLine();
-        Contact contact = new Contact(firstName, lastName, address, city, state, zip,
-                phoneNumber, email);
+        Contact contact = new Contact(
+                firstName, lastName, address,
+                city, state, zip,
+                phoneNumber, email
+        );
         addressBook.addContact(contact);
+
+        // -------- UC-3 : Edit Contact --------
+        System.out.println("\nEdit Existing Contact");
+        System.out.print("Enter First Name to Edit : ");
+        String editFirstName = myScan.nextLine();
+        System.out.print("Enter Last Name to Edit : ");
+        String editLastName = myScan.nextLine();
+        System.out.print("Enter New Address : ");
+        String newAddress = myScan.nextLine();
+        System.out.print("Enter New City : ");
+        String newCity = myScan.nextLine();
+        System.out.print("Enter New State : ");
+        String newState = myScan.nextLine();
+        System.out.print("Enter New Zip : ");
+        String newZip = myScan.nextLine();
+        System.out.print("Enter New Phone Number : ");
+        String newPhoneNumber = myScan.nextLine();
+        System.out.print("Enter New Email : ");
+        String newEmail = myScan.nextLine();
+        addressBook.editContact(
+                editFirstName, editLastName,
+                newAddress, newCity, newState,
+                newZip, newPhoneNumber, newEmail
+        );
     }
 }
