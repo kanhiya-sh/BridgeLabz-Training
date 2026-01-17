46c47e624768e448eac3e3beb78e0a4c3fc29313
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-17 11:09:34 +0530

    [kanhiya] Refactor. Included getter and setter to update details

--- oops-practice/gcr-codebase/address-book-system/Contact.java
+++ oops-practice/gcr-codebase/address-book-system/Contact.java
@@ -20,5 +20,30 @@ class Contact {
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
 }
 
