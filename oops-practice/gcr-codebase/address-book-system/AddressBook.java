cb0c248e81f7528a4ef1417acde9f3ceaa41d557
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-01-17 11:11:06 +0530

    [kanhiya] Refactor. Added an edit method using name

--- oops-practice/gcr-codebase/address-book-system/AddressBook.java
+++ oops-practice/gcr-codebase/address-book-system/AddressBook.java
@@ -8,4 +8,20 @@ public class AddressBook {
         contactList.add(contact);
         System.out.println("Contact Added Successfully !!");
     }
+//    to update the details using name
+    public void editContact(String firstName, String lastName, String address, String city,
+                            String state, String zip, String phoneNumber, String email) {
+        for(Contact contact : contactList) {
+            if(contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+                contact.setAddress(address);
+                contact.setCity(city);
+                contact.setState(state);
+                contact.setZip(zip);
+                contact.setPhoneNumber(phoneNumber);
+                contact.setEmail(email);
+                System.out.println("Contact updated successfully !!");
+                return;
+            }
+        }
+    }
 }
