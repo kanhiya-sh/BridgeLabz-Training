9414ae7ca83f0df7f9e40a63049e1a410b5f093b
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-02-02 16:12:27 +0530

    [kanhiya] Add. InvokePrivateMethod using reflection

---
+++ collections-practice/gcr-codebase/java-reflection/InvokePrivateMethod.java
@@ -0,0 +1,19 @@
+package BridgeLabz_Day37_Reflection;
+
+import java.lang.reflect.*;
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+public class InvokePrivateMethod {
+    public static void main(String[] args) throws Exception {
+        Calculator calc = new Calculator();
+        Class<?> cls = calc.getClass();
+        Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+        method.setAccessible(true);
+        int result = (int) method.invoke(calc, 5, 4);
+        System.out.println("Result: " + result);
+    }
+}
