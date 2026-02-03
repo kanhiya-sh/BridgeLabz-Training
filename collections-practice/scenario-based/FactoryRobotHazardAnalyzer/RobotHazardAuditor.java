003b13e3036ce444eb05b65dc5b198a61838b23f
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-02-03 16:25:02 +0530

    [kanhiya] Add. FactoryRobotHazardAnalyzer Question

---
+++ collections-practice/scenario-based/FactoryRobotHazardAnalyzer/RobotHazardAuditor.java
@@ -0,0 +1,40 @@
+package BridgeLabz_Day38_FactoryRobotHazardAnalyzer;
+
+public class RobotHazardAuditor {
+
+    public double calculateHazardRisk(double armPrecision, int workerDensity, String machineryState)
+            throws RobotSafetyException {
+
+        if (armPrecision < 0.0 || armPrecision > 1.0) {
+            throw new RobotSafetyException("Error: Arm precision must be 0.0-1.0");
+        }
+
+        if (workerDensity < 1 || workerDensity > 20) {
+            throw new RobotSafetyException("Error: Worker density must be 1-20");
+        }
+
+        double machineRiskFactor;
+
+        switch (machineryState) {
+            case "worn":
+                machineRiskFactor = 1.3;
+                break;
+
+            case "faulty":
+                machineRiskFactor = 2.0;
+                break;
+
+            case "critical":
+                machineRiskFactor = 3.0;
+                break;
+
+            default:
+                throw new RobotSafetyException("Error: Unsupported machinery state");
+        }
+
+        double hazardRisk =
+                ((1.0 - armPrecision) * 15.0) + (workerDensity * machineRiskFactor);
+
+        return hazardRisk;
+    }
+}
