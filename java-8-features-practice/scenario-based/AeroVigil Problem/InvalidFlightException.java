e1508ce526a2cac1a368aaa7d2707b73a6ada444
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-02-05 21:53:47 +0530

    [kanhiya] Add. AeroVigil Problem

---
+++ java-8-features-practice/scenario-based/AeroVigil Problem/InvalidFlightException.java
@@ -0,0 +1,7 @@
+package BridgeLabz_Day40_AeroVigilProblem;
+
+public class InvalidFlightException extends Exception {
+    public InvalidFlightException(String message) {
+        super(message);
+    }
+}
