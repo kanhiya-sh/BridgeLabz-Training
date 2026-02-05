1941d5ace8d4e178fd89489b682343eb86dbf7c9
kanhiya-sh <sharmakanhiya288@gmail.com>
2026-02-05 21:57:44 +0530

    [kanhiya] Removed. AeroVigil problem files

--- java-8-features-practice/scenario-based/AeroVigil Problem/InvalidFlightException.java
+++
@@ -1,7 +0,0 @@
-package BridgeLabz_Day40_AeroVigilProblem;
-
-public class InvalidFlightException extends Exception {
-    public InvalidFlightException(String message) {
-        super(message);
-    }
-}
